6d43e35c480b629523844fddfd62b6590dcd587d
LunaLii <48501012+LunaLii@users.noreply.github.com>
2019-04-01 16:30:30 +1300

    Add files via upload

--- duplication_demo1.py
+++ duplication_demo1.py
@@ -1,6 +1,6 @@
 # Refactoring Workbook Chapter 6 Duplication Challenge 1 (Exercise 14)
 
-# Step 3 - write get_times DocTests in an individual text file
+# Step 4 - more Pythonic
 
 
 class MissingPropertiesException(Exception):
@@ -8,29 +8,30 @@ class MissingPropertiesException(Exception):
 
 
 def get_times(**kargs):
-    if ("interval" in kargs) == 0:
+    if "interval" not in kargs:
         raise MissingPropertiesException("monitor interval")
     value = kargs["interval"]
-
-    if (value <= 0):
+    if value <= 0:
         raise MissingPropertiesException("monitor interval > 0")
     check_interval = value
 
-    if ("duration" in kargs) == 0:
+    if "duration" not in kargs:
         raise MissingPropertiesException("duration")
     value = kargs["duration"]
-    if (value <= 0):
+    if value <= 0:
         raise MissingPropertiesException("duration > 0")
-    if ((value % check_interval) != 0):
+
+    if (value % check_interval) != 0:
         raise MissingPropertiesException("duration % check_interval")
     monitor_time = value
 
-    if ("departure" in kargs) == 0:
+    if "departure" not in kargs:
         raise MissingPropertiesException("departure offset")
     value = kargs["departure"]
-    if (value <= 0):
+    if value <= 0:
         raise MissingPropertiesException("departure > 0")
-    if ((value % check_interval) != 0):
+
+    if (value % check_interval) != 0:
         raise MissingPropertiesException("departure % check_interval")
     departure_offset = value
 
