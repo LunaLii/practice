c5e64496d283cb65abe54b751d4dc347df49271e
LunaLii <48501012+LunaLii@users.noreply.github.com>
2019-04-01 16:43:19 +1300

    Add files via upload

--- duplication_demo1.py
+++ duplication_demo1.py
@@ -1,38 +1,57 @@
 # Refactoring Workbook Chapter 6 Duplication Challenge 1 (Exercise 14)
 
-# Step 4 - more Pythonic
+# Step 5 - extract duplicated code by extract method
 
 
 class MissingPropertiesException(Exception):
     pass
 
 
-def get_times(**kargs):
-    if "interval" not in kargs:
-        raise MissingPropertiesException("monitor interval")
-    value = kargs["interval"]
+def get_value(kargs, target, message1, message2=None):
+    if target not in kargs:
+        raise MissingPropertiesException(message1)
+    value = kargs[target]
     if value <= 0:
-        raise MissingPropertiesException("monitor interval > 0")
-    check_interval = value
+        if not message2:
+            raise MissingPropertiesException(message1 + " > 0")
+        else:
+            raise MissingPropertiesException(message2 + " > 0")
+    return value
 
-    if "duration" not in kargs:
-        raise MissingPropertiesException("duration")
-    value = kargs["duration"]
-    if value <= 0:
-        raise MissingPropertiesException("duration > 0")
+
+def get_times(**kargs):
+    #    if not "interval" in kargs:
+    #        raise MissingPropertiesException("monitor interval")
+    #    value = kargs["interval"]
+    #    if value <= 0:
+    #        raise MissingPropertiesException("monitor interval > 0")
+
+    #    check_interval = value
+    check_interval = get_value(kargs, "interval", "monitor interval")
+
+    #    if not "duration" in kargs:
+    #        raise MissingPropertiesException("duration")
+    #    value = kargs["duration"]
+    #    if value <= 0:
+    #        raise MissingPropertiesException("duration > 0")
+
+    value = get_value(kargs, "duration", "duration")
 
     if (value % check_interval) != 0:
         raise MissingPropertiesException("duration % check_interval")
+
     monitor_time = value
 
-    if "departure" not in kargs:
-        raise MissingPropertiesException("departure offset")
-    value = kargs["departure"]
-    if value <= 0:
-        raise MissingPropertiesException("departure > 0")
+    #    if not "departure" in kargs:
+    #        raise MissingPropertiesException("departure offset")
+    #    value = kargs["departure"]
+    #    if value <= 0:
+    #        raise MissingPropertiesException("departure > 0")
 
+    value = get_value(kargs, "departure", "departure offset", "departure")
     if (value % check_interval) != 0:
         raise MissingPropertiesException("departure % check_interval")
+
     departure_offset = value
 
     result = []
